ab7a0d56e5376b41d4ae2845e84e3469838da3df
Stephen Schrauger <git@schrauger.com>
2019-10-14 10:43:34 -0400

    commit

--- tertiary_backup.py
+++ tertiary_backup.py
@@ -18,16 +18,20 @@ def module_exists(module_name):
 if module_exists("config"):
 	import config
 else:
-	print("Please set up the config.py file. Copy 'sample.config.py' to 'config.py' and modify the default values.")
+	log.error("Please set up the config.py file. Copy 'sample.config.py' to 'config.py' and modify the default values.")
 	sys.exit(2)
 
 # check for verbose
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+parser.add_argument("-d", "--debug", help="max output verbosity", action="store_true")
 args = parser.parse_args()
 if args.verbose:
-	log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+	log.basicConfig(format="%(levelname)s: %(message)s", level=log.INFO)
 	log.info("Verbose output.")
+elif args.debug:
+	log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+	log.info("Debug output.")
 else:
 	log.basicConfig(format="%(levelname)s: %(message)s")
 
@@ -35,37 +39,57 @@ else:
 
 def main():
 	sites = config.sites
-	for site, site_details in sites:
+	for site, site_details in sites.items():
 		run_backup(site, site_details, config.local_storage_path)
-	send_email()
+#	send_email()
 	return 0
 
 def run_backup(site_name, site_details, local_storage_path):
 	local_site_path = local_storage_path + "/" + site_name + "/"
+	# 0. Make sure destination exists.
+	log.info("Creating local path")
+	mkdir(local_site_path)
 
 	# 1. rsync all files to local, including the database dump
-	print("Acquiring latest backup")
+	log.info("Acquiring latest backup")
 	rsync_download(site_details['rsync_dir'], local_site_path, site_details['rsync_user'], site_details['rsync_host'])
 
 	# 2. upload all web files to tertiary via ftp
-	print("Uploading files")
-	ftp_upload(local_site_path + "/web/", site_details['ftp_dir'], site_details['ftp_user'], site_details['ftp_pass'], site_details['ftp_host'])
+	log.info("Uploading files")
+	scp_upload(local_site_path + "/web/", site_details['ftp_dir'], site_details['ftp_user'], site_details['ftp_pass'], site_details['ftp_host'])
 
 	# 3. overwrite database on tertiary
-	print("Uploading database")
-	db_overwrite_tertiary(local_site_path + "/db/", site_details['db_schema'], site_details['db_user'], site_details['db_pass'], site_details['db_host'])
+#	log.info("Uploading database")
+#	db_overwrite_tertiary(local_site_path + "/db/", site_details['db_schema'], site_details['db_user'], site_details['db_pass'], site_details['db_host'])
+
+def mkdir(local_site_path):
+	mkdir_command = ['mkdir', '-p', local_site_path]
+	log.debug(mkdir_command)
+	subprocess.call(mkdir_command)
 
 # note: must use public/private key authentication. use a .ssh/config file to specify a private key if not using default ssh id_rsa
-def rsync_download(remote_latest_backup_location, local_web_dir_location, user, host):
+def rsync_download(remote_latest_backup_location, local_root_dir_location, user, host):
 	remote_location = user + '@' + host + ":" + remote_latest_backup_location
-	rsync_command = ['rsync', '-a', '--delete-during', remote_location, local_root_file_location]
-	log.info("Rsyncing files to local machine from " + remote_location)
+	if args.debug:
+		rsync_command = ['rsync', '-avxP', '--delete-during', '--exclude', 'wp-config.php', remote_location, local_root_dir_location]
+	else:
+		rsync_command = ['rsync', '-a', '--ignore-errors', '--delete-during', '--exclude', 'wp-config.php', remote_location, local_root_dir_location]
+	log.info("Rsyncing files (excluding wp-config.php) to local machine from " + remote_location)
+	log.debug(rsync_command)
 	subprocess.call(rsync_command)
+	delete_wpconfig_command = ['rm', '-f', local_root_dir_location + '/web/wp-config.php']
+	log.debug("Removing wp-config.php file if exists")
+	log.debug(delete_wpconfig_command)
+	subprocess.call(delete_wpconfig_command)
 
-def ftp_upload(local_web_dir_location, ftp_web_dir_location, user, password, host):
-	ftp_command = ['lftp', '-e', 'mirror -R ' + local_web_dir_location + ' ' + ftp_web_dir_location, '-u', user + ',' + password, host]
-	log.info("Uploading FTP files to " + ftp_web_dir_location + " on host " + host)
-	subprocess.call(ftp_command)
+def scp_upload(local_web_dir_location, scp_web_dir_location, user, password, host):
+	if args.debug:
+		scp_command = ['sshpass', '-p', password, 'scp', '-r', '-v', local_web_dir_location, user + '@' + host + ":'" + scp_web_dir_location + "'"]
+	else:
+		scp_command = ['sshpass', '-p', password, 'scp', '-r', local_web_dir_location, user + '@' + host + ":'" + scp_web_dir_location + "'"]
+	log.info("Uploading FTP files to " + scp_web_dir_location + " on host " + host)
+	log.debug(scp_command)
+	subprocess.call(scp_command)
 
 # takes the standard db dump, prepends drop schema, and uploads via mysql
 def db_overwrite_tertiary(local_db_dir_location, schema, user, password, host):
@@ -90,10 +114,10 @@ def db_get_dump_filename(local_db_dir_location):
 	for file in os.listdir(local_db_dir_location):
 		if file.endswith(".sql"):
 			sql_dump = os.path.join(local_db_dir_location, file)
-			log.info("Found sql dump at " + sql_dump
-			return(sql_dump) #return path to the first sql file found
+			log.info("Found sql dump at " + sql_dump)
+			return sql_dump #return path to the first sql file found
 	log.error("No .sql file located in " + local_db_dir_location)
-	return null #no .sql file detected in the directory given
+	return #no .sql file detected in the directory given
 
 def db_prepend_drop_schema(db_dump_file, schema):
 	# Prepend the database we want to overwrite.
@@ -143,7 +167,7 @@ Subject: %s
 	p.write(message)
 	status = p.close()
 	if status:
-		print("Sendmail exit status", status)
+		log.info("Sendmail exit status", status)
 
 main() # run the script
 
